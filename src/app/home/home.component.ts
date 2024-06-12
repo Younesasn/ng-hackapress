0ff19db815d092c3e127fa28e71e49d98b69925f
ASSANI Younes <younesassani@gmail.com>
2024-06-12 23:08:33 +0200

    add team

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,11 +4,12 @@ import { StatsComponent } from './stats/stats.component';
 import { ProductsComponent } from './products/products.component';
 import { WhyUsComponent } from './why-us/why-us.component';
 import { PlansComponent } from './plans/plans.component';
+import { TeamComponent } from './team/team.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, StatsComponent, ProductsComponent, WhyUsComponent, PlansComponent],
+  imports: [HeroComponent, StatsComponent, ProductsComponent, WhyUsComponent, PlansComponent, TeamComponent],
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
