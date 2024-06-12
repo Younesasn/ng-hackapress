7f11648bd443f620a1d5ba17db885bda063da2d0
ASSANI Younes <younesassani@gmail.com>
2024-06-12 22:30:58 +0200

    add plans

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,12 @@ import { HeroComponent } from './hero/hero.component';
 import { StatsComponent } from './stats/stats.component';
 import { ProductsComponent } from './products/products.component';
 import { WhyUsComponent } from './why-us/why-us.component';
+import { PlansComponent } from './plans/plans.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, StatsComponent, ProductsComponent, WhyUsComponent],
+  imports: [HeroComponent, StatsComponent, ProductsComponent, WhyUsComponent, PlansComponent],
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
---
+++ src/app/home/plans/plans.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plans',
+  standalone: true,
+  imports: [],
+  templateUrl: './plans.component.html'
+})
+export class PlansComponent {
+
+}
