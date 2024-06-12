b28bff15df4f58f52261fc2e68c850b302ef54e2
ASSANI Younes <younesassani@gmail.com>
2024-06-12 14:27:13 +0200

    add product section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
 import { StatsComponent } from './stats/stats.component';
+import { ProductsComponent } from './products/products.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, StatsComponent],
+  imports: [HeroComponent, StatsComponent, ProductsComponent],
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
