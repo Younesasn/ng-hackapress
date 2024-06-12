91f01bb1c5dc688ad409567569d9e78306b38778
ASSANI Younes <younesassani@gmail.com>
2024-06-12 20:34:22 +0200

    add why-us

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
 import { StatsComponent } from './stats/stats.component';
 import { ProductsComponent } from './products/products.component';
+import { WhyUsComponent } from './why-us/why-us.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, StatsComponent, ProductsComponent],
+  imports: [HeroComponent, StatsComponent, ProductsComponent, WhyUsComponent],
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
