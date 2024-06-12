c2f27447a2174df2f7e4e75d250479b1ca241d69
ASSANI Younes <younesassani@gmail.com>
2024-06-12 12:04:12 +0200

    add stats & change README

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
+import { StatsComponent } from './stats/stats.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent],
+  imports: [HeroComponent, StatsComponent],
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
