880d7b6d10c29fc9775fa9cf72788b8ce1ff388a
ASSANI Younes <younesassani@gmail.com>
2024-06-28 12:41:12 +0200

    add login form (not-functional)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ServicesComponent } from './services/services.component';
 import { ProductsComponent } from './products/products.component';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
+    {path: "login", component: LoginComponent},
     {path: "services", component: ServicesComponent},
     {path: "products", component: ProductsComponent},
-    {path: "", component: HomeComponent}
+    {path: "", component: HomeComponent},
+    {path: "**", component: PageNotFoundComponent}
 ];
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  public form: FormGroup = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required],
+    }),
+  });
+
+  login() {
+    if (this.form.value.email === 'you@you.com' && this.form.value.password === 'you') {
+      console.log('connecté !');
+    } else {
+      console.log('erreur');
+    }
+  }
+}
