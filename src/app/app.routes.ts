dadb490a84aad6d0acb4a39d85962c26ff3ce0c5
ASSANI Younes <younesassani@gmail.com>
2024-07-14 16:00:09 +0200

    disable products page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,6 @@ import { ProductsComponent } from './products/products.component';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { ProfileComponent } from './profile/profile.component';
-// import { AuthGuard } from './shared/auth.guard';
 import { RegisterComponent } from './register/register.component';
 import { CartComponent } from './cart/cart.component';
 import { authGuard } from './shared/guards/auth.guard';
@@ -15,7 +14,7 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'services', component: ServicesComponent },
-  { path: 'products', component: ProductsComponent },
+  // { path: 'products', component: ProductsComponent },
   { path: 'cart', component: CartComponent },
   { path: '', component: HomeComponent },
   { path: '**', component: PageNotFoundComponent },
