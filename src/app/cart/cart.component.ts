c4df7002a1ec440749b676473ced83a55c47adff
ASSANI Younes <younesassani@gmail.com>
2024-08-13 21:38:06 +0200

    update cart component

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -2,8 +2,8 @@ import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { OneItem, Service } from '../shared/entities';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { Subscription, forkJoin } from 'rxjs';
 import { CartService } from '../shared/services/cart.service';
+import { environment } from '../../environments/environment.development';
 
 @Component({
   selector: 'app-cart',
@@ -17,19 +17,13 @@ export class CartComponent implements OnInit, OnDestroy {
   taxes: number = 5.99;
   service!: Service;
   cartService = inject(CartService);
+  urlImage: string = environment.urlImage;
 
   ngOnInit(): void {
     if (localStorage.getItem('cart')) {
       this.cart = JSON.parse(localStorage.getItem('cart') || '[]');
       this.total = this.cart.reduce((acc, item) => acc + item.price, 0);
-      this.test();
     }
-  }
-
-  test() {
-    this.cartService.getServiceByUrl('http://localhost:8000/api/services/1').subscribe((service: Service) => {
-      this.service = service;
-    });
   }
 
   deleteItem(item: OneItem) {
