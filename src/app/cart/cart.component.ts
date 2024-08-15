9dc2224e5bc30f0fb196fb2b9a0efb54df25371e
ASSANI Younes <younesassani@gmail.com>
2024-08-15 23:08:49 +0200

    update cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -4,6 +4,9 @@ import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { CartService } from '../shared/services/cart.service';
 import { environment } from '../../environments/environment.development';
+import { AuthService } from '../shared/services/auth.service';
+import { HttpClient } from '@angular/common/http';
+import { forkJoin, map } from 'rxjs';
 
 @Component({
   selector: 'app-cart',
@@ -17,20 +20,48 @@ export class CartComponent implements OnInit, OnDestroy {
   taxes: number = 5.99;
   service!: Service;
   cartService = inject(CartService);
+  isLogged = inject(AuthService).isLogged();
+  http = inject(HttpClient);
   urlImage: string = environment.urlImage;
+  url: string = environment.url;
 
   ngOnInit(): void {
     if (localStorage.getItem('cart')) {
       this.cart = JSON.parse(localStorage.getItem('cart') || '[]');
       this.total = this.cart.reduce((acc, item) => acc + item.price, 0);
     }
+    this.updateCartItem();
+  }
+
+  updateCartItem() {
+    const requests = this.cart.map((item) => {
+      const matterRequest = this.http.get<any>(environment.url + item.matter);
+      const productRequest = this.http.get<any>(environment.url + item.product);
+      const serviceRequest = this.http.get<any>(environment.url + item.service);
+
+      return forkJoin([matterRequest, productRequest, serviceRequest]).pipe(
+        map(([matter, product, service]) => {
+          item.matter = matter.name;
+          item.product = product.name;
+          item.service = service.name;
+          return item;
+        })
+      );
+    });
+
+    forkJoin(requests).subscribe((updatedCart: OneItem[]) => {
+      this.cart = updatedCart;
+    });
   }
 
   deleteItem(item: OneItem) {
-    this.cart = this.cart.filter((cartItem) => cartItem !== item);
-    localStorage.setItem('cart', JSON.stringify(this.cart));
+    if (this.cart.length > 1) {
+      this.cart = this.cart.filter((cartItem) => cartItem !== item);
+      localStorage.setItem('cart', JSON.stringify(this.cart));
+    } else {
+      this.cart = [];
+      localStorage.removeItem('cart');
+    }
   }
-
-  ngOnDestroy(): void {
-  }
+  ngOnDestroy(): void {}
 }
