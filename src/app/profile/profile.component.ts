2d462db6920473e13b87999f52dd458f35373c4d
ASSANI Younes <younesassani@gmail.com>
2024-08-19 19:53:33 +0200

    update profile page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,11 +1,115 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { OrderService } from '../shared/services/order.service';
+import { OneItem, Order, User } from '../shared/entities';
+import { Subscription, forkJoin, map } from 'rxjs';
+import { AuthService } from '../shared/services/auth.service';
+import { UserService } from '../shared/services/user.service';
+import { CommonModule } from '@angular/common';
+import { ItemService } from '../shared/services/item.service';
+import { environment } from '../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
-  templateUrl: './profile.component.html'
+  imports: [CommonModule],
+  templateUrl: './profile.component.html',
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit, OnDestroy {
+  userService = inject(UserService);
+  orderService = inject(OrderService);
+  authService = inject(AuthService);
+  itemService = inject(ItemService);
+  http = inject(HttpClient);
 
+  user!: User;
+  dataUser!: Subscription;
+  orders: Order[] = [];
+  token = this.authService.getDecodedToken();
+  urlImage = environment.urlImage;
+
+  dataModal: OneItem[] = [];
+
+  ngOnInit() {
+    this.getDataUser();
+    console.log({ orders: this.orders });
+  }
+
+  getDataUser() {
+    this.dataUser = this.userService
+      .getUserById(this.token.user_id)
+      .subscribe((data: User) => {
+        this.user = data;
+        data.orders.forEach((order) => {
+          this.orderService.getOrderByUri(order).subscribe((data: Order) => {
+            this.orders.push(data);
+          });
+        });
+      });
+  }
+
+  openModal(items: string[], id: number) {
+    const currentItems = this.dataModal.map((item) => item);
+    const newItems = items;
+
+    if (JSON.stringify(currentItems) !== JSON.stringify(newItems)) {
+      this.dataModal = [];
+      items.forEach((item: string) => {
+        this.itemService.getItemByUri(item).subscribe((data: OneItem) => {
+          this.dataModal.push(data);
+
+          const requests = this.dataModal.map((item) => {
+            const matterRequest = this.http.get<any>(
+              environment.url + item.matter
+            );
+            const productRequest = this.http.get<any>(
+              environment.url + item.product
+            );
+            const serviceRequest = this.http.get<any>(
+              environment.url + item.service
+            );
+            const statusRequest = this.http.get<any>(
+              environment.url + item.status
+            );
+
+            return forkJoin([
+              matterRequest,
+              productRequest,
+              serviceRequest,
+              statusRequest,
+            ]).pipe(
+              map(([matter, product, service, status]) => {
+                item.matter = matter.name;
+                item.product = product.name;
+                item.service = service.name;
+                item.status = status.name;
+                return item;
+              })
+            );
+          });
+
+          forkJoin(requests).subscribe((updatedCart: OneItem[]) => {
+            this.dataModal = updatedCart;
+          });
+        });
+      });
+    }
+
+    let modal = document.getElementById('modal') as HTMLElement;
+    modal.classList.remove('hidden');
+    modal.classList.add('flex');
+    modal.classList.add('opacity-100');
+  }
+
+  closeModal() {
+    let modal = document.getElementById('modal') as HTMLElement;
+    modal.classList.add('hidden');
+    modal.classList.add('opacity-0');
+    modal.classList.remove('flex');
+    modal.classList.remove('opacity-100');
+  }
+
+  ngOnDestroy() {
+    this.dataUser.unsubscribe();
+  }
 }
