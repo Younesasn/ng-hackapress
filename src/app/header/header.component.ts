81358574401609d555f1efd056f907a15acc1bea
ASSANI Younes <younesassani@gmail.com>
2024-08-21 18:50:46 +0200

    add logout

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,21 @@
 import { Component, Inject, Input, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { ButtonComponent } from "../components/button/button.component";
+import { ButtonComponent } from '../components/button/button.component';
 import { User } from '../shared/entities';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
-    selector: 'app-header',
-    standalone: true,
-    templateUrl: './header.component.html',
-    imports: [RouterLink, ButtonComponent, CommonModule]
+  selector: 'app-header',
+  standalone: true,
+  templateUrl: './header.component.html',
+  imports: [RouterLink, ButtonComponent, CommonModule],
 })
 export class HeaderComponent {
   @Input() user?: User;
+  authService = inject(AuthService);
+
+  logout() {
+    this.authService.logout();
+  }
 }
