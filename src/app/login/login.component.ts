2e3d9d84acfdbb4f58cc7894ef9601c7e7462bf1
ASSANI Younes <younesassani@gmail.com>
2024-09-17 01:52:43 +0200

    test

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -39,6 +39,7 @@ export class LoginComponent implements OnInit {
       const { username, password } = this.form.value;
       this.authService.login({ username, password }).subscribe(
         (token) => {
+          console.log(token);
           this.authService.saveToken(token);
           this.router.navigate(['/profile']);
         },
